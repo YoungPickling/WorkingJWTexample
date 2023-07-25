4194b1d2634f7875078732567ea291b2f7c82d93
Maksim Pavlenko <maksimas.pavlenko@gmail.com>
2023-07-25 09:11:08 +0300

    Initial commit

---
+++ src/main/java/lt/max/auth/RegisterRequest.java
@@ -0,0 +1,18 @@
+package lt.max.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+}
---
+++ src/main/java/lt/max/user/Role.java
@@ -0,0 +1,6 @@
+package lt.max.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
